90ec825aa1da516d9e27301b5bea72ffb070d1e9
Vicki Hamm <vicki.cello@gmail.com>
2018-04-05 08:09:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return(`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown(days, holiday) {
-  return(`It's parseInt(${days}) days until ${holiday}!`)
+function holidayCountdown(parseInt(days), holiday) {
+  return(`It's ${days} days until ${holiday}!`)
 }
